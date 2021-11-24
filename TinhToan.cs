c5753e919f5b499ead21ac12a0a83da884ed3cfb
tat1310 <trananhtoan2k@gmail.com>
2021-11-24 14:55:31 +0700

    test commit

---
+++ TinhToan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TranAnhToan_1961_De7.Models
+{
+    public class TinhToan
+    {
+        public string mahocsinh { get; set; }
+        public string hoten { get; set; }
+        public string lop { get; set; }
+        public string diachi { get; set; }
+        public string songaynghihoc { get; set; }
+        public string hocphi { get; set; }
+    }
+}
